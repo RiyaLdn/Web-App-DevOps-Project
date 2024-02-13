fef69f31d668bd97f74027ca3f948da0e3ac8c1f
Riya Longia <riyalongia@gmail.com>
2024-02-13 14:16:47 +0000

    Commiting added variables, main and output file configurations for the networking-module file for terraform

---
+++ aks-terraform/modules/networking-module/main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">=2.0"
+    }
+  }
+}
+
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "aks_vnet" {
+    name   = "aks_vnet"
+    resource_group_name = azurerm_resource_group.aks_rg.name
+    location            = azurerm_resource_group.aks_rg.location
+    address_space       = var.vnet_address_space
+}
+
+resource "azurerm_subnet" "control_plane_subnet" {
+  name                 = "control-plane-subnet"
+  resource_group_name  = azurerm_resource_group.aks_rg.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+}
+
+resource "azurerm_subnet" "worker_node_subnet" {
+  name                 = "worker-node-subnet"
+  resource_group_name  = azurerm_resource_group.aks_rg.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "aks_nsg" {
+  name                = "aks-nsg"
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  location            = azurerm_resource_group.aks_rg.location
+}
+
+resource "azurerm_network_security_rule" "kube_apiserver_rule" {
+  name                        = "kube-apiserver-rule"
+  resource_group_name         = azurerm_resource_group.aks_rg.name
+  network_security_group_name = azurerm_network_security_group.aks_nsg.name
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_address_prefix       = "*"  
+  source_port_range           = "*"
+  destination_address_prefix  = "*"
+  destination_port_range      = "6443"
+}
+
+resource "azurerm_network_security_rule" "ssh_rule" {
+  name                        = "ssh-rule"
+  resource_group_name         = azurerm_resource_group.aks_rg.name
+  network_security_group_name = azurerm_network_security_group.aks_nsg.name
+  priority                    = 1002
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_address_prefix       = "*"  
+  source_port_range           = "*"
+  destination_address_prefix  = "*"
+  destination_port_range      = "22"
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.aks_rg.name
+}
+
+output "vnet_name" {
+  value = azurerm_virtual_network.aks_vnet.name
+}
